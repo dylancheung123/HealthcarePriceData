2bd5d48ca7282383a0ff7984fdb739cfc9b8c9eb
dylan <cheund3@gmail.com>
2021-02-20 15:54:10 -0500

    Fixing buttons not working and connecting to auth

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <div className='body'>
-      Home
-    </div>
-  )
-}
+const Home = () => (
+  <div className='body'>
+    Home
+  </div>
+)
 
 export default Home
